4db8fc5cdec7b0a4c03db6092796316de4553016
yas <yasuhiro.kato@d2cid.co.jp>
2022-12-14 13:52:36 +0900

    todoを作った

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,30 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import { useState } from "react";
 
 export default function Home() {
+
+  const [todos, setTodos] = useState<string[]>([])
+
+  const addTodo = () => {
+    setTodos([...todos, ""]);
+  };
+
+  const editTodo = (insertIndex: number, newValue: string) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((value, index) => (index == insertIndex ? newValue : value))
+    )
+  }
+
+  const deleteTodo = (deleteIndex: number) => {
+    setTodos((prevTodos) =>
+      prevTodos.filter((value, index) => index != deleteIndex)
+    )
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,59 +34,27 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        
+        <ul>
+        {todos.map((todo) => (
+          <li key={todo}>{todo}</li>
+        ))}
+        </ul>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div>
+          <Button variant="contained" onClick={addTodo}>Add</Button>
         </div>
+        
+        {todos.map((todo, index) => (
+          <div className={styles.text_line}>
+            <TextField className={styles.text_field} id="text_field" label="" variant="standard" onChange={e => editTodo(index, e.target.value) }/>
+            <Button variant="contained" onClick={() => deleteTodo(index)}>delete</Button>
+          </div>
+        ))}
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+          Powered by {'yas'}
       </footer>
     </div>
   )
