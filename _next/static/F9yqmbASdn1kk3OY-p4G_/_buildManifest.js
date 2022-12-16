5686112a245a627eae5dfdf9e24c2e09c647159d
dot-yas <dot-yas@users.noreply.github.com>
2022-12-16 10:15:24 +0000

    deploy: ebf32d6456f03c9bec40d3fef25783cc6bc2a759

--- _next/static/F9yqmbASdn1kk3OY-p4G_/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/484-746daa8fab6dc1f0.js","static/css/13c80c23a03a405d.css","static/chunks/pages/index-41fb2e36989a6e45.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/WCpmvYhMgttGqBoTnv7hR/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/484-746daa8fab6dc1f0.js","static/css/13c80c23a03a405d.css","static/chunks/pages/index-aa3d417703ea7b94.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
